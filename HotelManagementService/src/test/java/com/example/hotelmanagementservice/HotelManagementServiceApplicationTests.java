5767347823412502bcfa9bc09bd5e34d2a13a0fb
Belma Muftic <bmuftic1@etf.unsa.ba>
2019-04-14 20:42:02 +0200

    Adding unit test

--- HotelManagementService/src/test/java/com/example/hotelmanagementservice/HotelManagementServiceApplicationTests.java
+++ HotelManagementService/src/test/java/com/example/hotelmanagementservice/HotelManagementServiceApplicationTests.java
@@ -1,16 +1,42 @@
 package com.example.hotelmanagementservice;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HotelManagementServiceApplicationTests {
 
+
+    String umsPort = "8088";
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+
+    public void findUserFromUMS() throws URISyntaxException
+    {
+        String userID = "1";
+
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = "http://localhost:" + umsPort + "/user/" + userID;
+
+        URI uri = new URI(baseUrl);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        System.out.println(result.getStatusCode());
+        Assert.assertTrue(result.getStatusCode().equals(200));
+
     }
 
 }
