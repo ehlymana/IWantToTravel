3db7dc497297b9915668b5c18239ba126875adea
Nkulovic <nkulovic1@etf.unsa.ba>
2019-05-07 22:40:29 +0200

    API gateway added

---
+++ zuul-proxy/src/main/java/com/nwt/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,22 @@
+package com.nwt.zuulproxy;
+
+import com.nwt.zuulproxy.filters.pre.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
+
+}
