6d240596fd9a4040064cc832aebdc7857d2edb7f
ehlymana <ekrupalija1@etf.unsa.ba>
2019-05-14 23:48:14 +0200

    Added app JARs. Added docker configurations (server).

--- App/src/main/java/com/nwt/hbs/HbsApplication.java
+++ App/src/main/java/com/nwt/hbs/HbsApplication.java
@@ -2,11 +2,12 @@ package com.nwt.hbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class HbsApplication {
+public class HbsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HbsApplication.class, args);
