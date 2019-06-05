0d14a935847b45ba0c0126832c5d994975d0ef64
Nkulovic <nkulovic1@etf.unsa.ba>
2019-06-06 01:40:10 +0200

    Added authorization server

---
+++ authorization/authorization-server/authorization-server-1/src/test/java/com/baeldung/UaaServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.baeldung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.baeldung.UaaServiceApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = UaaServiceApplication.class)
+public class UaaServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
